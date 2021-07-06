48d1d547b1133c4b0f57618dc6545e05a2f039f4
user <fn1919@mailg.denpa.ac.jp>
2021-07-06 14:59:02 +0900

    バグ修正完了本当の

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.FragmentSecondBinding
 import io.realm.Realm
@@ -70,6 +71,14 @@ class SecondFragment : Fragment() {
                             add(Calendar.MILLISECOND, -1)
                         }.time
                 ).findAll().sort("date")
+        val adapter=ScheduleAdapter(schedules)
+        binding.list.adapter=adapter
+        adapter.setOnItemClickListener { id->
+            id?.let {
+                val action=SecondFragmentDirections.actionToScheduleEditFragment(it)
+                findNavController().navigate(action)
+            }
+        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
