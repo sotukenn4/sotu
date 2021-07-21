7f98d61f458fc7ff3cd37849e84e135e7d322c52
user <tanksk1951@gmail.com>
2021-07-21 14:14:27 +0900

    天気コミット

--- app/src/main/java/com/example/myapplication/tenki.kt
+++ app/src/main/java/com/example/myapplication/tenki.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
@@ -13,10 +12,7 @@ import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.HandlerCompat
-import androidx.fragment.app.FragmentActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.lifecycle.ViewModel
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStream
@@ -25,14 +21,17 @@ import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
 import java.net.URL
 import java.util.concurrent.Executors
+
 //////天気くらす
 class tenki : AppCompatActivity() {
     companion object{
         private const val DEBUG_TAG="AsyncSample"
-        private const val WEATHERINFO_URL="https://api.openweathermap.org/data/2.5/weather?lang=ja"
+        private const val WEATHERINFO_URL="https://api.openweathermap.org/data/2.5/weather?lang=ja&units=metric"
         private const val APP_ID="83311ce95b6808b861ddd5e178b11b87"
+        //private const val APP_ID="cdc3c950d8b98d3fc80f4669f52a346a"
 
     }
+    //メイン
     private var _list: MutableList<MutableMap<String, String>> =mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +47,7 @@ class tenki : AppCompatActivity() {
         lvCityList.onItemClickListener=ListItemClickListener()
 
     }
-
+    //リスト
     private fun createList(): MutableList<MutableMap<String, String>> {
         var list:MutableList<MutableMap<String, String>> = mutableListOf()
         var city= mutableMapOf("name" to "愛知", "q" to "Aichi")
@@ -56,6 +55,8 @@ class tenki : AppCompatActivity() {
         city= mutableMapOf("name" to "大阪", "q" to "Osaka")
         list.add(city)
         city= mutableMapOf("name" to "岐阜", "q" to "Gifu")
+        list.add(city)
+        city= mutableMapOf("name" to "名古屋", "q" to "Nagoya")
         list.add(city)
         return list
 
@@ -117,12 +118,33 @@ class tenki : AppCompatActivity() {
             val weatherJSONArray=rootJSON.getJSONArray("weather")
             val weatherJSON=weatherJSONArray.getJSONObject(0)
             val weather=weatherJSON.getString("description")
-            val telop="${cityName}の天気"
-            val desc="現在は${weather}です。\n緯度は${latitude}度で軽度は${longitude}です。"
+            //追加
+            val weatherJSONArraysub = rootJSON.getJSONObject("main")
+            val degree = weatherJSONArraysub.getString("temp")
+            //気温変換関数呼び出し
+            val chan = ChangeNum(degree)
+            val telop="現在の  \n"+"${cityName}"+"の天気"
+            //追加
+            val desc="天気 ：${weather} "
+            // 緯度は${latitude}度で軽度は${longitude}です。
+            //追加
+            val temp ="気温は              \n" + "${chan}" + "度です。";
+
             val tvWeatherTelop=findViewById<TextView>(R.id.tvWeatherTelop)
             val tvWeatherDesc=findViewById<TextView>(R.id.tvWeatherDesc)
+            //追加
+            val tvWeatherDegrees = findViewById<TextView>(R.id.tvWeatherdegree)
             tvWeatherTelop.text=telop
             tvWeatherDesc.text=desc
+            //追加
+            tvWeatherDegrees.text=temp
+        }
+
+        // 追加　気温切り捨て変換用関数
+        fun ChangeNum(degree: String):Double{
+            var x = degree
+            var y = Math.round((x.toDouble()) * 10.0) / 10.0
+            return y
         }
     }
 
@@ -140,4 +162,9 @@ class tenki : AppCompatActivity() {
             }
         }
     }
+
+    //予報用
+
+
+
 }
